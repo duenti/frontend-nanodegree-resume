35c5b1e1ab7753f99186caadf53274f9bf667b7e
Néli Fonseca <nelijfjr@gmail.com>
2014-12-02 20:54:20 -0200

    Intruções pra editar header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,19 +1,5 @@
-//Printar nome com JQuery
-$("#main").append("Neli Fonseca");
+var formattedName = HTMLheaderName.replace("%data%", "Neli Fonseca");
+var formattedRole = HTMLheaderRole.replace("%data%", "Desenvolvedor");
 
-//Converter Velocidade da luz
-vLuzM = 299792458; //Em mestros por segundo
-vluzC = vLuzM * 100; //Velocidade da Luz em cm/s
-vLuzNC = vluzC * (1/1000000000); //Velocidade da Luz em cm/ns
-$("#main").append("<br>");
-$("#main").append(vLuzNC);
-
-//Testando Variaveis
-var awesomeToughts = "Im Neli and I am AWESOME!"
-
-//console.log(awesomeToughts); //Imprime no Console
-
-//String.replace
-var funToughts = awesomeToughts.replace("AWESOME", "FUN");
-
-console.log(funToughts);
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
