49d4032bbe302c876119ad8666bf637e12381f4c
Néli Fonseca <nelijfjr@gmail.com>
2014-12-02 04:17:26 -0200

    Add conversão de velocidade da Luz (Fim lição 0)

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,9 @@
+//Printar nome com JQuery
 $("#main").append("Neli Fonseca");
+
+//Converter Velocidade da luz
+vLuzM = 299792458; //Em mestros por segundo
+vluzC = vLuzM * 100; //Velocidade da Luz em cm/s
+vLuzNC = vluzC * (1/1000000000); //Velocidade da Luz em cm/ns
+$("#main").append("<br>");
+$("#main").append(vLuzNC);
